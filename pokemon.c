9889a37ef4f31d1570bb7399b449fb664f93ed9c
Jake Zaia <jzaia@stuy.edu>
2018-01-06 14:00:12 -0500

    Add csv for pokemon data

--- pokemon.c
+++ pokemon.c
@@ -1,6 +1,37 @@
 #include "sharedheader.h"
 
 
+/*
+  The following 2 functions are for reading csvs. They are not 100% original code, but were adapted from the post of StackOverflow user sehe.
+ */
+const char* getfield(char* line, int num){
+    const char* tok;
+    for (tok = strtok(line, ",");
+            tok && *tok;
+            tok = strtok(NULL, ",\n"))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
+
+//TODO
+int set_stats(){
+    FILE* stream = fopen("input", "r");
+
+    char line[1024];
+    while (fgets(line, 1024, stream))
+    {
+        char* tmp = strdup(line);
+        printf("Field 3 would be %s\n", getfield(tmp, 3));
+        // NOTE strtok clobbers tmp
+        free(tmp);
+    }
+    return 0;
+}
+
+
 struct Pokemon* construct_pokemon(int choice) {
   struct Pokemon *p = calloc(sizeof(struct Pokemon), 1); //needs default 0s
 
