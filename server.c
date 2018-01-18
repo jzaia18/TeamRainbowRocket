7be2f9a50c402dff19bb701282e4a20c1a2cef7f
TakingTheL <yliu6@stuy.edu>
2018-01-18 15:34:49 -0500

    SAVE ME PIOTR

--- server.c
+++ server.c
@@ -1,13 +1,28 @@
 #include "sharedheader.h"
 
-void server(){
+void server(int ip, int port){
+
+  int listen_socket;
   int clients[2];
-
-  // wait for client 1 to connect
+  char buffer[BUFFER_SIZE];
+  int client_counter;
   
-  // wait for client 2 to connect
+  // create & bind a server socket to IP & port
+  listen_socket ;
 
-  // fork off to subserver
+  //do next two things at the same time
+    
+  // wait for clients to connect
+  clients[client_counter] = ;
+
+  // match the two clients for a match
+  
+  // fork off the two to subserver
+  if(0 == fork()){
+    //subserver
+  } else {
+    //server goes back to waiting/matching
+  } 
 
   // establish connection to both clients
 
