159e17331a652cb0071f214d4e3b13aaad9c6bbf
Rihui Zheng <rzheng4@stuy.edu>
2018-01-19 15:36:17 -0500

    did stoof

--- teambuildering.c
+++ teambuildering.c
@@ -10,9 +10,9 @@ char *learnset_lookup(int poke_id) {
   struct stat ap;
   stat(POKEDATA_CSV, &ap);
   char file[ap.st_size+15];
-  printf("Reading file\n");
+  //printf("Reading file\n");
   read(fd, file, sizeof(file));
-  printf("Read file\n");
+  //printf("Read file\n");
 
   char *buf = file;
 
@@ -20,32 +20,56 @@ char *learnset_lookup(int poke_id) {
   while (i-- > 0) strsep(&buf, "\n"); // Run down the entries in the list
 
   char *line = strcpy(malloc(1000), strsep(&buf, "\n"));
-  printf("Line: %s\n", line);
+  //printf("Line: %s\n", line);
+
+  char *tmp = line;
 
   i = POKEDATA_LEARNSET_INDEX;
-  while (i-- > 0) strsep(&line, ",");
+  while (--i > 0) strsep(&line, ",");
 
-  char *ret = strcpy(malloc(MAX_LEARNSET_SIZE*4), strsep(&line, ","));
-  //*strchr(ret, '"') = 0;
+  char *ret = strcpy(malloc(MAX_LEARNSET_SIZE*4), strsep(&line, ",") + 1);
+  *strchr(ret, '"') = 0;
 
-  printf("Finished running\n");
-  free(file);
-  free(line);
+  //printf("Finished running\n");
+  free(tmp);
   close(fd);
 
-  printf("returning\n");
+  //printf("returning\n");
 
-  return NULL;
+  return ret;
 }
 
 int *get_learnset(int poke_id) {
-  malloc(MAX_LEARNSET_SIZE + 1);
+  //getting data
+  int *ret = malloc(MAX_LEARNSET_SIZE + 1);
+  char *moves = learnset_lookup(poke_id);
+  char *tmp = moves;
 
-  return NULL;
+  char *c;
+  
+  int i = 0;
+
+  while(moves){
+    c = strsep(&moves, ";");
+    ret[i] = atoi(c);
+    i++;
+  }
+  //printf("test");
+
+  free(tmp);
+  return ret;
 }
 
 
 int main() {
-  learnset_lookup(150);
+  //learnset_lookup(150);
+  
   //printf("%s\n", learnset_lookup(150));
+  int *data = get_learnset(150);
+  
+  int i = 5;
+  while(i--){
+    printf("%d\n",data[i]);
+  }
+  
 }
