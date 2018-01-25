76bb7b653c3cae352cb867cad92888a231636a4a
Piotr Milewski <pmilewski@stuy.edu>
2018-01-24 19:21:47 -0500

    fixed issues with server_setup and finished server_connect

--- networking.c
+++ networking.c
@@ -17,9 +17,9 @@ void error_check( int i, char *s ) {
     int Socket Descriptor
 ~~~~~~~~~~~~~~~~~~~~~~~~~*/
 int *server_setup() {
-  int n;
 
   //used for incrementation and storage
+  int n;
   int i = 0;
   char s[64];
   
@@ -27,7 +27,7 @@ int *server_setup() {
   int numOfSockets = 2;
 
   //storage for the socket descriptors
-  int sd[numOfSockets];
+  int *sd = calloc(numOfSockets, sizeof(int));
 
   //create the sockets
   for (; i < numOfSockets; i++){
@@ -45,12 +45,12 @@ int *server_setup() {
     getaddrinfo(NULL, 5001 + i, hints, &results); //NULL -> local address 
 
     //bind socket[i] to address and port 
-    n = bind( sd[0], results->ai_addr, results->ai_addrlen ); 
+    n = bind( sd[i], results->ai_addr, results->ai_addrlen ); 
     error_check( n, "server binding" ); 
     printf("[server] socket bound\n"); 
 
     //set socket to listen state 
-    n = listen(sd, 10); 
+    n = listen(sd[i], 10); 
     error_check( n, "server listen" ); 
     printf("[server] socket in listen state\n");
 
@@ -61,27 +61,37 @@ int *server_setup() {
   return sd;
 }
 
-/*=========================
+/*~~~~~~~~~~~~~~~~~~~~~~~~~
   server_connect
-  args: int sd
-  sd should refer to a socket in the listening state
-  run the accept call
-  returns the socket descriptor for the new socket connected
-  to the client.
-  =========================*/
+    run the accept call
+  Arguments: int sd
+    sd should refer to a socket in the listening state
+  Return:
+    The socket descriptor for the new socket connected to the client.
+  ~~~~~~~~~~~~~~~~~~~~~~~~~*/
 int server_connect(int sd) {
+  int client_sock;
+  struct sockaddr_storage client_address;
+  socklen_t sock_size = sizeof(client_address);
 
+  client_socket = accept(sd, (struct sockadd *)&client_address, &sock_size);
+  error_check(client_socket, "server acceptance");
+
+  return client_socket;
 }
 
-/*=========================
+/*~~~~~~~~~~~~~~~~~~~~~~~~~
   client_setup
   args: int * to_server
   to_server is a string representing the server address
   create and connect a socket to a server socket that is
   in the listening state
   returns the file descriptor for the socket
-  =========================*/
+  ~~~~~~~~~~~~~~~~~~~~~~~~~*/
 int client_setup(char * server) {
+
+  //used for incrementation and storage
+  int sd, i;
 
   //create the socket
 
