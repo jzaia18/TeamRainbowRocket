51146ef6a30ea72a4baca6670ba58fe96ae77b4b
Piotr Milewski <pmilewski@stuy.edu>
2018-01-24 20:16:14 -0500

    finished client_setup

--- networking.c
+++ networking.c
@@ -82,11 +82,11 @@ int server_connect(int sd) {
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~
   client_setup
-  args: int * to_server
-  to_server is a string representing the server address
-  create and connect a socket to a server socket that is
-  in the listening state
-  returns the file descriptor for the socket
+    create and connect a socket to a server socket that is in the listening state
+  Arguments: int * to_server
+    to_server is a string representing the server address
+  Return:
+    The file descriptor for the socket
   ~~~~~~~~~~~~~~~~~~~~~~~~~*/
 int client_setup(char * server) {
 
@@ -94,13 +94,23 @@ int client_setup(char * server) {
   int sd, i;
 
   //create the socket
+  sd = socket (AF_INET, SOCK_STREAM, 0);
+  error_check( sd, "client socket" );
 
   //run getaddrinfo
-  /* hints->ai_flags not needed because the client
-     specifies the desired address. */
-
+  struct addrinfo * hints, * results;
+  hints = (struct addrinfo *)calloc(1, sizeof(struct addrinfo));
+  hints->ai_family = AF_INET;  //IPv4
+  hints->ai_socktype = SOCK_STREAM;  //TCP socket
+  getaddrinfo(server, PORT, hints, &results);  
 
   //connect to the server
   //connect will bind the socket for us
+  i = connect( sd, results->ai_addr, results->ai_addrlen );
+  error_check( i, "client connect" );
   
+  free(hints);
+  freeaddrinfo(results);
+
+  return sd;
 }
