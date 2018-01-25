c31b5c5cd475805ad10e52c824b2b7aca6140c0c
TakingTheL <yliu6@stuy.edu>
2018-01-25 08:58:26 -0500

    worked on a selecting client

--- client.c
+++ client.c
@@ -1,12 +1,36 @@
 #include "sharedheader.h"
 
-int clientsetup() {
-
+int main(int argc, char ** argv){
   int server_socket;
-  char buffer[BUFFER_SIZE];
-  
-  //
+  char buffer[64];
 
+  if (argc == 2)
+    server_socket = client_setup(argv[1]);
+  else
+    server_socket = client_setup(/* INSERT IP & PORT HERE */);
 
+  while (1){
+    
+    printf("enter text to send: ");
+    
+    //select() modifies read_fds
+    //we must reset it at each iteration
+    FD_ZERO(&read_fds);
+    FD_SET(STDIN_FILENO, &read_fds); //add stdin to fd set
+    FD_SET(server_socket, &read_fds); //add socket to fd set
+    
+    select(server_socket + 1/* I honestly don't know what this param does */, &read_fds, NULL, NULL, NULL);
+    
+    if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+      fgets(buffer, sizeof(buffer), stdin);
+      *strchr(buffer, '\n') = 0;
+      write(server_socket, buffer, sizeof(buffer));
+      //Do we really want to read here?
+    }
 
+    if (FD_ISSET(server_socket, &read_fds)) {
+      read(server_socket, buffer, sizeof(buffer));
+      printf("recieved: %s\n", buffer);
+    }
+  }
 }
